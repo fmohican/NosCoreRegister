a09fb9fcddb43525131b63f9ca35ea04fbe0b5d9
Fmohican <florinmohican@gmail.com>
2019-10-25 13:04:29 +0300

    backend working... still in progress...

--- core/core.php
+++ core/core.php
@@ -59,4 +59,70 @@ class core {
     }
   }
 
+  public function sign_gen() {
+    global $signature_secret;
+    return hash_hmac("sha3-512", hash("SHA256", ip2long($this->self_ip())), $signature_secret);
+  }
+
+  public function sign_check($sign) {
+    $verifity = $this->sign_gen();
+    return (strcmp($sign, $verifity) ? true : false);
+  }
+
+  private function check_username_exist($username) {
+    global $dbhost, $dbname, $dbpass, $dbport, $dbuser; #
+    $db = new Medoo\Medoo([
+    'database_type' => 'pgsql',
+    'database_name' => $dbname,
+    'server' => $dbhost,
+    'port' => $dbport,
+    'username' => $dbuser,
+    'password' => $dbpass
+    ]);
+    return ($db->has("Accounts", ["name" => $username]) ? true : false);
+  }
+  public function self_ip() {
+    $cloudflareIPRanges = array(
+      '204.93.240.0/24',
+      '204.93.177.0/24',
+      '199.27.128.0/21',
+      '173.245.48.0/20',
+      '103.21.244.0/22',
+      '103.22.200.0/22',
+      '103.31.4.0/22',
+      '141.101.64.0/18',
+      '108.162.192.0/18',
+      '190.93.240.0/20',
+      '188.114.96.0/20',
+      '197.234.240.0/22',
+      '198.41.128.0/17',
+      '162.158.0.0/15'
+    );
+    if(isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+      $validCFRequest = false;
+      foreach($cloudflareIPRanges as $range){
+        if($this->ip_in_range($_SERVER['REMOTE_ADDR'], $range)) {
+          $validCFRequest = true;
+          break;
+        }
+      }
+      if($validCFRequest)
+        return $_SERVER["HTTP_CF_CONNECTING_IP"];
+      else
+        return $_SERVER['REMOTE_ADDR'];
+    }
+    else
+      return $_SERVER['REMOTE_ADDR'];
+  }
+  private function ip_in_range($ip, $range) {
+    if ( strpos( $range, '/' ) == false ) {
+      $range .= '/32';
+    }
+    list( $range, $netmask ) = explode( '/', $range, 2 );
+    $range_decimal = ip2long( $range );
+    $ip_decimal = ip2long( $ip );
+    $wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+    $netmask_decimal = ~ $wildcard_decimal;
+    return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+  }
 }
